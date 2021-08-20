4c8931c29de3cadb9bff940c01399f9e2041dcd8
Ryan Summerlin <summerlinryan@gmail.com>
2021-08-19 23:07:56 -0400

    Make bootloader use tty mode to output HELLO

--- bootloader.asm
+++ bootloader.asm
@@ -1,5 +1,15 @@
-loop:
-    jmp loop
+mov ah, 0x0e ; use teletype output mode
+mov al, 'H'
+int 0x10 ; write character in al
+mov al, 'E'
+int 0x10 ; write character in al
+mov al, 'L'
+int 0x10 ; write character in al
+int 0x10 ; write character in al
+mov al, 'O'
+int 0x10 ; write character in al
+
+jmp $ ; jump to current address (i.e. infinite loop)
 
 ; add 0 bytes up until last 2 bytes of bootloader with need to be 0xAA55
 ; to tell the BIOS that this disk is bootable
