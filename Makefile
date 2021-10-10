70db60d9d89a5e1ed0eaf1934bf850a770e0d900
Ryan Summerlin <summerlinryan@gmail.com>
2021-10-09 22:39:22 -0400

    Load and call into kernel

--- Makefile
+++ Makefile
@@ -2,6 +2,14 @@ bootloader:
 	nasm -f bin bootloader.asm -o bootloader.bin
 	qemu-system-i386 bootloader.bin
 
+run: 
+	qemu-system-i386 os-image.bin
+
+kernel:
+	gcc -ffreestanding -c kernel.c -o kernel.o
+	ld -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
+	cat bootloader.bin kernel.bin > os-image.bin
+
 debug:
 	nasm -f bin bootloader.asm -o bootloader.bin
 	qemu-system-i386 -s -S bootloader.bin
--- bootloader.asm
+++ bootloader.asm
@@ -1,30 +1,44 @@
-[org 0x7c00] ; memory offset of the bootloader (because of the BIOS)
+[org 0x7c00]
 
+KERNEL_OFFSET equ 0x1000 ; This is the memory offset to which we will load our kernel
+
+mov [BOOT_DRIVE], dl 
 mov bp, 0x9000
 mov sp, bp
-
-mov bx, MSG_REAL_MODE
-call print_string
-
-call switch_to_pm
+mov bx, MSG_REAL_MODE 
+call print_string 
+call load_kernel
+call switch_to_pm 
 jmp $
 
-%include "./print_string.asm"
-%include "./gdt.asm"
-%include "./print_string_pm.asm"
-%include "./switch_to_pm.asm"
+%include "print_string.asm"
+%include "disk_load.asm"
+%include "gdt.asm"
+%include "print_string_pm.asm"
+%include "switch_to_pm.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print_string
+    mov bx, KERNEL_OFFSET 
+    mov dh, 15
+    mov dl, [BOOT_DRIVE] 
+    call disk_load 
+    ret
 
 [bits 32]
+; This is where we arrive after switching to and initialising protected mode.
 BEGIN_PM:
     mov ebx, MSG_PROT_MODE
-    call print_string_pm
-
+    call print_string_pm 
+    call KERNEL_OFFSET
     jmp $
 
-; Global variables
-MSG_REAL_MODE: db "Started in 16-bit Real Mode", 0
-MSG_PROT_MODE: db "Successfully landed in 32-bit Protected Mode", 0
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory.", 0
 
-times 510-($-$$) db 0 ; pad with 0s until the last 2 bytes of the 512 byte boot sector 
-dw 0xaa55 ; magic number that tells the BIOS this is bootable
-
+times 510-($-$$) db 0
+dw 0xaa55
